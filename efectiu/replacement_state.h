0dfefe721314efc05896bb73530780b86949e27d
Ping Wang <freesummer@compute-linux1.cs.tamu.edu>
2015-12-06 22:05:18 -0600

    eighth commit

--- efectiu/replacement_state.h
+++ efectiu/replacement_state.h
@@ -148,7 +148,7 @@ private:
     INT32  Get_LRU_Victim( UINT32 setIndex );  
     INT32  Get_My_Victim( UINT32 setIndex); 
     void   UpdateLRU( UINT32 setIndex, INT32 updateWayID ); 
-    void   UpdateMyPolicy( UINT32 setIndex, UINT32 samplerSetIndex, INT32 updateWayID );
+    void   UpdateMyPolicy( UINT32 setIndex, INT32 updateWayID );
 
 public:
 
